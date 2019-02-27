722fa9e740804622bcd428fd8385599d979a714c
Eric Schreiter <mail@shter.de>
2019-02-27 12:51:18 +0100

    Offline
    offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,39 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    {
+      plugins: [
+        {
+          resolve: `gatsby-plugin-manifest`,
+          options: {
+            name: "Eric Schreiter",
+            short_name: "shter.de",
+            start_url: "/",
+            background_color: "#6b37bf",
+            theme_color: "#6b37bf",
+            // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+            // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+            display: "standalone",
+            icon: "src/img/icon.png", // This path is relative to the root of the site.
+            // An optional attribute which provides support for CORS check.
+            // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+            // Any invalid keyword or empty string defaults to `anonymous`
+            crossOrigin: `use-credentials`,
+          },
+        },
+      ]
+    },
+    {
+      plugins: [
+        {
+          resolve: `gatsby-plugin-manifest`,
+          options: {
+            ...
+          }
+        },
+        'gatsby-plugin-offline'
+      ]
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
